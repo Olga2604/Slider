8df3c47c47a7f8b7bc4dd94001d0a00bc542de8b
Olga <30666069+Olga2604@users.noreply.github.com>
2019-02-10 19:35:55 +0300

    Create HomePage.vue

---
+++ HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <some-component />
+  <image-slider :dotNav="true /> //pass a prop which tells to the slider component to use dot navigation
+</template>
+
+<script>
+import SomeComponent from '../blocks/SomeElements';
+import ImageSlider from '../elements/ImageSlider';
+
+export default {
+  components: {
+    ImageSlider,
+    SomeComponent,
+  },
+};
+</script>
